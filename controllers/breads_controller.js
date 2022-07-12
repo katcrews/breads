9feb691eafd3df7b4809bc9d2845872e9c91c726
Kat Crews <katfreitas808@gmail.com>
2022-07-11 18:51:57 -0700

    delete route complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -9,7 +9,7 @@ breads.get('/', (req, res) => {
       breads: Bread
     }
   )
-// res.send(Bread)
+  // res.send(Bread)
 });
 
 // NEW
@@ -22,19 +22,21 @@ breads.get('/new', (req, res) => {
 breads.get('/:arrayIndex', (req, res) => {
   if (Bread[req.params.arrayIndex]) {
     res.render('Show', {
-      bread:Bread[req.params.arrayIndex]
+      bread: Bread[req.params.arrayIndex],
+      index: req.params.arrayIndex,
     })
   } else {
-    res.send('404')
+    res.render('404')
   }
-});
+})
+
 
 // CREATE
 breads.post('/', (req, res) => {
   if (!req.body.image) {
     req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
   }
-  if(req.body.hasGluten === 'on') {
+  if (req.body.hasGluten === 'on') {
     req.body.hasGluten = true
   } else {
     req.body.hasGluten = false
@@ -43,6 +45,11 @@ breads.post('/', (req, res) => {
   res.redirect('/breads')
 });
 
+// DELETE
+breads.delete('/:indexArray', (req, res) => {
+  Bread.splice(req.params.indexArray, 1)
+  res.status(303).redirect('/breads')
+})
 
 
 module.exports = breads
