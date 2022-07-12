c1dd5f9a4d8e930b511c2719f36e1168ad762e8a
Kat Crews <katfreitas808@gmail.com>
2022-07-11 20:35:37 -0700

    edit and update routes complete

--- views/show.jsx
+++ views/show.jsx
@@ -17,11 +17,10 @@ function Show({ bread, index }) {
             </p>
             <img src={bread.image} alt={bread.name} />
             <li><a href="/breads">Go home</a></li>
-
             <form action={`/breads/${index}?_method=DELETE`} method="POST">
-                <input type='submit' value="DELETE" />
+                <input type='submit' value="DELETE" />   
             </form>
-
+            <a href={`/breads/${index}/edit`}><button>Edit</button></a>
         </Default>
     )
 }
