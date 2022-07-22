d125c54a04e0cc67ea12e62a2c6aa7e68bbfc4ef
Kat Crews <katfreitas808@gmail.com>
2022-07-22 12:12:27 -0700

    part 2 helper methods. connection still not working

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -32,23 +32,29 @@ breads.get('/:id', (req, res) => {
 });
 
 // EDIT
-breads.get('/:indexArray/edit', (req, res) => {
-  res.render('edit', {
-    bread: Bread[req.params.indexArray],
-    index: req.params.indexArray
-  })
+breads.get('/:id/edit', (req, res) => {
+  Bread.findById(req.params.id) 
+    .then(foundBread => { 
+      res.render('edit', {
+        bread: foundBread 
+      })
+    })
 })
 
 // UPDATE
-breads.put('/:arrayIndex', (req, res) => {
-  if (req.body.hasGluten === 'on') {
+breads.put('/:id', (req, res) => {
+  if(req.body.hasGluten === 'on'){
     req.body.hasGluten = true
   } else {
     req.body.hasGluten = false
   }
-  Bread[req.params.arrayIndex] = req.body
-  res.redirect(`/breads/${req.params.arrayIndex}`)
+  Bread.findByIdAndUpdate(req.params.id, req.body, { new: true }) 
+    .then(updatedBread => {
+      console.log(updatedBread) 
+      res.redirect(`/breads/${req.params.id}`) 
+    })
 })
+
 
 // CREATE
 breads.post('/', (req, res) => {
@@ -65,9 +71,12 @@ breads.post('/', (req, res) => {
 });
 
 // DELETE
-breads.delete('/:indexArray', (req, res) => {
-  Bread.splice(req.params.indexArray, 1)
-  res.status(303).redirect('/breads')
+breads.delete('/:id', (req, res) => {
+  Bread.findByIdAndDelete(req.params.id) 
+    .then(deletedBread => { 
+      res.status(303).redirect('/breads')
+    })
 })
+
 
 module.exports = breads
