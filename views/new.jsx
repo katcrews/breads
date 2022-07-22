1f0fcab1343d96de2855f1d345649edd1c6fd628
Kat Crews <katfreitas808@gmail.com>
2022-07-22 13:28:39 -0700

    be-9 multiple models. server connection error persists

--- views/new.jsx
+++ views/new.jsx
@@ -1,7 +1,7 @@
 const React = require('react')
 const Default = require('./layouts/Default')
 
-function New() {
+function New(bakers) {
     return (
         <Default>
             <h2>Add a new bread</h2>
@@ -19,6 +19,11 @@ function New() {
                     id="image" />
                 <label htmlFor="baker">Baker</label>
                 <select name="baker" id="baker">
+                    {bakers.map((baker) => {
+                        return (
+                            <option value={baker.id} key={baker.id}>{baker.name}</option>
+                        )
+                    })}
                     <option value="Rachel">Rachel</option>
                     <option value="Monica">Monica</option>
                     <option value="Joey">Joey</option>
